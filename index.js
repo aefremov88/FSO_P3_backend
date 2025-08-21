abc058cdf8ce50827c3d5229c384cf533b5f563f
Anton Efremov <aefremov88@gmail.com>
2025-08-21 15:24:39 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
 app.use(express.static('dist'))
-
-app.use(cors())
 
 app.use(express.json())
 
@@ -23,28 +22,6 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234347"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 // ===GET=========================================================
 
@@ -53,45 +30,64 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(result => {
+      if (result) {
+        response.json(result)
+      } else {response.status(404).end()}
+    })
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${Date(Date.now())}</p>`
-)
+app.get('/info', async (request, response) => {
+  Person.find({})
+  .then(result => 
+    response.send(
+      `<p>Phonebook has info for ${result.length} people</p>
+      <p>${Date(Date.now())}</p>`
+    )
+  )
+})
+
+// ===PUT=========================================================
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
 })
 
 // ===DELETE======================================================
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // ===POST========================================================
 
-const generateId = () => {
-    const min = 0;
-    const max = 100000;
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', async (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -106,23 +102,45 @@ app.post('/api/persons', (request, response) => {
     return;
   }
   
-  if (persons.find((person) => person.name === body.name)) {
-    response.status(400).json({ 
-    error: 'name must be unique'});
-    return;
-  }
-
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
+  })
+
+  person.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
+})
+
+// ===UNKNOWN URL FALLBACK=========================================
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+// ===ERROR HANDLER================================================
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'malformatted id' })
+  } 
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  persons = persons.concat(person)
+  next(error)
+}
 
-  response.json(person)
-})
+app.use(errorHandler)
+
+
+// ===LISTENNING PIPELINE LAUNCH==================================
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {

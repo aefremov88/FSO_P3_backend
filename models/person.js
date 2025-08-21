9db30765341c4ba6063d6ea32fa971a84fa449d7
Anton Efremov <aefremov88@gmail.com>
2025-08-21 15:35:36 -0400

    uibuild

--- models/person.js
+++ models/person.js
@@ -24,7 +24,7 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 8,
     validate: {
-        validator: ((v) => /\d{2,3}-\d+/.test(v)), 
+        validator: ((v) => /^\d{2,3}-\d+$/.test(v)), 
         message: props => `${props.value} is not a valid phone number!`
     },
     required: true
